6284cac5a204a02727830f78d520731e03e71189
George Captain <60938173+glcaptain00@users.noreply.github.com>
2025-06-11 10:08:26 -0500

    Upload bridge programs

---
+++ motionControlScripts/UMC-V2/LoRa Bridge/txbridge.py
@@ -0,0 +1,86 @@
+import time
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import board
+import adafruit_ssd1306
+import adafruit_rfm9x
+import threading
+import socket
+import select
+
+# Create the I2C interface.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# 128x32 OLED Display
+reset_pin = DigitalInOut(board.D4)
+display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+# Clear the display.
+display.fill(0)
+display.show()
+width = display.width
+height = display.height
+
+# Configure LoRa Radio
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+rfm9x.tx_power = 23
+prev_packet = None
+
+lora_tx = []
+lora_rx = []
+
+def check_lora():
+    global lora_rx, lora_tx, rfm9x
+    while True:
+        packet = rfm9x.receive()
+        if packet:
+            message = packet.decode('utf-8')
+            print(f"MESSAGE: {message}")
+            lora_rx += message
+
+        if len(lora_tx) > 0:
+            print(f"[LORA] Sending {len(lora_tx)} bytes!")
+            # rfm9x.send("".join(lora_tx).encode('utf-8'))
+            rfm9x.send(bytes(lora_tx))
+            lora_tx = []
+        
+
+
+# Start background thread
+lora_thread = threading.Thread(target=check_lora, daemon=True)
+lora_thread.start()
+
+
+HOST = '192.168.27.194'
+PORT = 12345
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock.bind((HOST, PORT))
+# sock.setblocking(False)
+# sock.settimeout(1.0)
+# sock.listen()
+display.fill(0)
+display.text('Running Bridge', 35, 0, 1)
+display.show()
+try:
+    # conn, addr = sock.accept()
+    sock.connect((HOST, PORT))
+    while True:
+        readable, _, _ = select.select([sock], [], [], 0)
+        if readable:
+            data_bytes = sock.recv(1024)
+            if data_bytes:
+                print(f"DATA: {data_bytes.decode('utf-8')}")
+                lora_tx += data_bytes
+        if len(lora_rx) > 0:
+            print(f"LoRa -> Ethernet: {lora_rx}")
+            sock.send("".join(lora_rx).encode("utf-8"))
+            lora_rx = []
+        
+
+
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    print("Exiting...")
+    sock.close()
